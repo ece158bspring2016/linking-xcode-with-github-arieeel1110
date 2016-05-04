39b7d0bd9ff7c8b06d97e31bbe28ed1f6742123f
Ariel <arieeel1110@gmail.com>
2016-05-04 14:55:43 -0700

    xcode

---
+++ Ariel/Ariel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ariel
+//
+//  Created by Ariel on 5/4/16.
+//  Copyright © 2016 Ariel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
